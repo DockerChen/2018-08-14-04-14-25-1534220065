1c83925154fc5fa52820f5f0ee38a3c381cfff95
woniu <18810913096@163.com>
2019-05-21 22:34:53 +0800

    add new files

--- main/main.js
+++ main/main.js
@@ -1,4 +1,43 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+function getResult(str) {
+    var lcd1 = ['._.', '...', '._.', '._.', '...', '._.', '._.', '._.', '._.', '._.'];
+    var lcd2 = ['|.|', '..|', '._|', '._|', '|_|', '|_.', '|_.', '..|', '|_|', '|_|'];
+    var lcd3 = ['|_|', '..|', '|_.', '._|', '..|', '._|', '|_|', '..|', '|_|', '..|'];
+    var result = '';
+    var count = 3;
+
+    for (var i = 0; i < str.length; i++) {
+        if (i == 0) {
+            result += lcd1[parseInt(str[i])];
+        } else {
+            result += " " + lcd1[parseInt(str[i])];
+        }
+    }
+    result += '\n';
+
+    for (var i = 0; i < str.length; i++) {
+        if (i == 0) {
+            result += lcd2[parseInt(str[i])];
+        } else {
+            result += " " + lcd2[parseInt(str[i])];
+        }
+    }
+    result += '\n';
+
+    for (var i = 0; i < str.length; i++) {
+        if (i == 0) {
+            result += lcd3[parseInt(str[i])];
+        } else {
+            result += " " + lcd3[parseInt(str[i])];
+        }
+    }
+    result += '\n';
+
+
+    return result;
+}
+
+module.exports = function main(number) {
+
+    var str = number.toString();
+    return getResult(str);
 };
